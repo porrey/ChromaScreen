4385574f92ec003e59461170381367c89a2cfd2c
Daniel Porrey <porrey@users.noreply.github.com>
2026-01-16 18:34:54 -0600

    Document rotation matrices in ChromaScreen-rotate.sh
    Added comments for rotation matrices used in display settings.

--- scripts/ChromaScreen-rotate.sh
+++ scripts/ChromaScreen-rotate.sh
@@ -10,5 +10,14 @@ done
 # Rotate the Touch Display 2 output
 xrandr --output DSI-1 --mode 720x1280 --rotate left --pos 0x0
 
+# Rotation	Matrix
+# 0°	1 0 0 0 1 0 0 0 1
+# 90° Clockwise	0 -1 1 1 0 0 0 0 1
+# 90° Counter-Clockwise	0 1 0 -1 0 1 0 0 1
+# 180° (Inverts X and Y)	-1 0 1 0 -1 1 0 0 1
+# invert Y	-1 0 1 1 1 0 0 0 1
+# invert X	-1 0 1 0 1 0 0 0 1
+# expand to twice the size horizontally	0.5 0 0 0 1 0 0 0 1
+
 # Rotate touch to match display (xrandr --rotate right)
 xinput set-prop 6 "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
